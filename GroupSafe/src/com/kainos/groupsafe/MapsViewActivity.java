6d5f2959c2e86e9f272306e338e057df72e9697e
Rhiannon Henry <rhenry872@gmail.com>
2014-02-20 15:51:14 +0000

    Showing users current location on map

--- GroupSafe/src/com/kainos/groupsafe/MapsViewActivity.java
+++ GroupSafe/src/com/kainos/groupsafe/MapsViewActivity.java
@@ -9,12 +9,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.kainos.groupsafe.utilities.GPSTracker;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsViewActivity extends Activity {
 
@@ -22,9 +28,8 @@ public class MapsViewActivity extends Activity {
 			.getLogger(MapsViewActivity.class.getName());
 
 	private GoogleMap googleMap;
+	MarkerOptions currentLocationMarker;
 	GPSTracker locationServices;
-	double lat;
-	double lng;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +43,60 @@ public class MapsViewActivity extends Activity {
 		try {
 			// Loading Map
 			initialiseMap();
+			createCurrentLocationMarker();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void createCurrentLocationMarker() {
+		if (currentLocationMarker == null) {
+			currentLocationMarker = new MarkerOptions();
+			getCurrentLocation();
+		} else {
+			getCurrentLocation();
+		}
+
+	}
+
+	private void getCurrentLocation() {
+		locationServices = new GPSTracker(MapsViewActivity.this);
+		if (locationServices.canGetLocation()) {
+			double lat = getMyLat();
+			double lng = getMyLng();
+			ParseUser currentUser = ParseUser.getCurrentUser();
+			String userObjectId = currentUser.getObjectId();
+			LOGGER.info("The Current User is: " + userObjectId);
+			LOGGER.info("" + userObjectId + ": Lat: " + lat + " Lng: " + lng);
+
+			String latitude = String.valueOf(lat);
+			String longitude = String.valueOf(lng);
+
+			
+			
+			ParseObject currentLocation = new ParseObject("Location");
+			currentLocation.put("currentLat", latitude);
+			currentLocation.put("currentLng", longitude);
+			currentLocation.put("userId", userObjectId);
+			try {
+				currentLocation.save();
+			} catch (ParseException e2) {
+				LOGGER.info("UNABLE TO SAVE NEW EMERGENCY CONTACT AT THIS TIME...");
+				e2.printStackTrace();
+			}
+			setMarker(lat, lng);
+		} else {
+			locationServices.showSettingAlert();
+		}
+	}
+
+	private void setMarker(double lat, double lng) {
+		LatLng currentPosition = new LatLng(lat, lng);
+		googleMap.addMarker(currentLocationMarker.position(currentPosition).title("Current Location").draggable(false));
+		
+		CameraPosition cameraPosition = new CameraPosition.Builder().target(currentPosition).zoom(12).build();
+		googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+		
 	}
 
 	private void initialiseMap() {
@@ -50,13 +106,22 @@ public class MapsViewActivity extends Activity {
 			if (googleMap == null) {
 				Toast.makeText(getApplicationContext(),
 						"Unable to create map..", Toast.LENGTH_SHORT).show();
-
 			}
 		}
 	}
-	
+
+	private double getMyLat() {
+		double latitude = locationServices.getLat();
+		return latitude;
+	}
+
+	private double getMyLng() {
+		double longitude = locationServices.getLng();
+		return longitude;
+	}
+
 	@Override
-	protected void onResume(){
+	protected void onResume() {
 		super.onResume();
 		initialiseMap();
 	}
